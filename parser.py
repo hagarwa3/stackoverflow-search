1478ba584f3b621318902e4dbd9a48a58613dd2f
Harshit Agarwal <hagarwa3@illinois.edu>
2016-05-02 23:16:24 -0500

    removed original parsers

--- parser.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('Posts.xml')
-root = tree.getroot()
-
-rows = tree.findall(".//row[@PostTypeId='2']")
-for elem in rows:
-    root.remove(elem)
-
-tree.write('cut.xml')
